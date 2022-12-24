0b05fb74482462e0bdb774f4cafd9ae908ae3ce8
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2022-12-24 15:49:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,18 +21,7 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.render('404', {title: "Page introuvable"})
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
+  res.render('404', {title: "Page not found"})
 });
 
 module.exports = app;
